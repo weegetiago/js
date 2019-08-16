7a1664ca261f9beccd11b867d6141d0d0fe6c23d
tiago.weege <tiago.weege@senior.com.br>
2019-08-16 17:44:11 -0300

    #87

--- funcao/classeVsFactory.js
+++ funcao/classeVsFactory.js
@@ -1,8 +1,22 @@
-class Pessoa {
+//classe
+class ClassePessoa {
     constructor(nome) {
         this.nome = nome
     }
     falar() {
-        console.log(`Meu nome é ${this.nome`})
+        console.log(`ClassePessoa: O nome é ${this.nome}`)
     }
 }
+const criarPessoa = new ClassePessoa('Joao')
+criarPessoa.falar()
+
+//funcao
+function FuncaoPessoa(nome) {
+    this.nome = nome
+
+    this.falar = function(){
+        console.log(`FuncaoPessoa: O nome é ${this.nome}`)
+    }
+}
+const kriarPessoa = new FuncaoPessoa('Joao')
+kriarPessoa.falar()
---
+++ funcao/iife.js
@@ -0,0 +1,5 @@
+//IIFE: funão imediatamente invocada.
+
+(function(){
+    console.log('É executada na hora')
+})()
