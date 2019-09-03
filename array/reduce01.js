ce598d41ced124ba001ec51032d556c561796625
weegetiago <weegetiago@gmail.com>
2019-09-03 20:59:53 -0300

    #116

---
+++ array/reduce01.js
@@ -0,0 +1,14 @@
+//REDUCE: Transforma um array em um único elemento.
+const alunos = [
+    { nome: 'Weege', nota: 7.3, bolsista: true },
+    { nome: 'Clemente', nota: 9.2, bolsista: true },
+    { nome: 'Ana', nota: 9.8, bolsista: false },
+    { nome: 'Amanda', nota: 8.7, bolsista: true },
+]
+
+console.log(alunos.map(a => a.nota)) //mostra as otas
+const somaNotas = alunos.map(a => a.nota).reduce(function (acumulador, atual) {
+    console.log('Soma anterior:',acumulador,',', 'Valor que sera somado:',atual) //mostra o valor atual mais o que será somado
+    return acumulador + atual
+})
+console.log('Soma das notas:',somaNotas)
