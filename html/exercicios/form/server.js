99057c31ff9012b4341af5e408ac92ae0a795d36
tiago.weege <tiago.weege@senior.com.br>
2019-09-24 17:50:05 -0300

    #183

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('Parabéns')
+    resp.send('Parabéns usuário incluido')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('Parabéns usuário alterado')
 })
 
 app.listen(3004)
